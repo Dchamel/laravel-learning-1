132a9395e487d65d7606277b755bde1449b4141b
Dchamel <dchamel@yandex.ru>
2021-12-30 03:57:02 +0400

    few small addings

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -17,4 +17,12 @@ class MainController extends Controller
     public function reviewPage() {
         return view('review');
     }
+
+    public function reviewCheck(Request $request) {
+        $valid = $request->validate([
+            'email' => 'required|min:4|max:100',
+            'subject' => 'required|min:4|max:100',
+            'message' => 'required|min:15|max:500'
+        ]);
+    }
 }
--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -3,9 +3,21 @@
 @section('title')Review Page @endsection
 
 @section('main_content')
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptatum itaque aliquid ex suscipit repellat at quia rem, debitis minima fugit repellendus, sit corporis rerum provident cum quod sequi? Error.
+
+    <h1>Review Page</h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
     <form action="post" action="{{ route('reviewCheck') }}">
+        @csrf
         <input type="email" name="email" id="email" placeholder="Input Email" class="form-control mb-2">
         <input type="text" name="subject" id="subject" placeholder="Input Subject" class="form-control mb-2">
         <textarea name="message" id="message" rows="4" placeholder="Input your Review here..." class="form-control mb-2"></textarea>
